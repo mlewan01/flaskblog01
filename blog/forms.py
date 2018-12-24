2d295e9c4dcdc54ba5bb8703322cee29314c3ae0
Unknown <le.wy@op.pl>
2018-12-24 03:54:54 +0000

    registration form saves hashed passwords to db

--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from blog.models import User, Post
 
 
 class RegistrationForm(FlaskForm):
@@ -9,6 +10,21 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
+
+    # def validate_field(self, field): # generic custom form validator
+    #     if True:
+    #         raise ValidationError('Validation Message')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Pleas choose different one')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken. Pleas choose different one')
+
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
